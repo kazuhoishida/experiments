64ff0331a7b9d033c9f212e13cb0a8ad7ef610e4
Kazuho Ishida <k10700110@gmail.com>
2022-07-03 20:38:44 +0900

    fix: :pencil2: typo

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -8,7 +8,7 @@ type Props = {
   }
 }
 
-const Cursol = ({isVisible, position}: Props) => {
+const Cursor = ({isVisible, position}: Props) => {
   const [scale, setScale] = useState(0)
   useEffect(() => {
     setScale(isVisible ? 100 : 0)
@@ -16,19 +16,19 @@ const Cursol = ({isVisible, position}: Props) => {
   return (
     <div
       className={`
-        absolute top-[var(--cursol-y)] left-[var(--cursol-x)] scale-[var(--cursol-scale)]
+        absolute top-[var(--cursor-y)] left-[var(--cursor-x)] scale-[var(--cursor-scale)]
         flex place-items-center place-content-center
         font-flex font-squash-h4 text-[17px]
         w-32 h-32 translate-x-[-4rem] translate-y-[-4rem] rounded-full overflow-hidden
         transition-transform duration-[400ms] backdrop-invert pointer-events-none
       `}
       style={{
-        '--cursol-x': `${position.x}px`,
-        '--cursol-y': `${position.y}px`,
-        '--cursol-scale': `${scale}%`,
+        '--cursor-x': `${position.x}px`,
+        '--cursor-y': `${position.y}px`,
+        '--cursor-scale': `${scale}%`,
       }}
     >VIEW</div>
   )
 }
 
-export default Cursol
+export default Cursor
