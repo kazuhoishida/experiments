aa34e624c3fb71eaa4470980674ddd7b1cd90778
Kazuho Ishida <kazuhoishida@Kazuhos-MacBook-Pro.local>
2022-07-27 15:19:22 +0900

    :pencil2: fix typo

--- components/CaretIcon.tsx
+++ components/CaretIcon.tsx
@@ -1,4 +1,4 @@
-const CarretIcon = () => {
+const CaretIcon = () => {
   return (
     <svg width="21" height="10" viewBox="0 0 21 10" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M0.363281 1.14771L10.5237 8.85242L20.0531 1.14771" stroke="white"/>
@@ -6,4 +6,4 @@ const CarretIcon = () => {
   )
 }
 
-export default CarretIcon
+export default CaretIcon
