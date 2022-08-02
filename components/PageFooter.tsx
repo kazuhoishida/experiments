474ccf17ff604ac0c186cb751621b25753d14db1
Kazuho Ishida <kazuhoishida@Kazuhos-MacBook-Pro.local>
2022-08-02 19:58:59 +0900

    :bug: fix footer navigation intersection observer

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -23,39 +23,35 @@ export const PageFooter = ({nav}: ProjectProps) => {
   },[])
 
   return (
-    <>
-      {pathname !== '/' && (
-        <footer id="page-footer" className={`pb-8`}>
-          <div className="px-8 md:p-0 md:w-9/10 mx-auto">
-            <div className="w-full h-px bg-black"></div>
+    <footer id="page-footer" className={`pb-8 hidden ${pathname === '/' ? 'hidden' : '!block'}`}>
+      <div className="px-8 md:p-0 md:w-9/10 mx-auto">
+        <div className="w-full h-px bg-black"></div>
+      </div>
+      <div className='md:w-9/10 md:mx-auto md:flex md:justify-between md:gap-x-4 md:items-center'>
+        <div className='md:flex md:justify-start md:gap-x-4 md:items-center'>
+          <div className="w-full md:w-auto flex place-content-center my-8">
+            <Logo />
           </div>
-          <div className='md:w-9/10 md:mx-auto md:flex md:justify-between md:gap-x-4 md:items-center'>
-            <div className='md:flex md:justify-start md:gap-x-4 md:items-center'>
-              <div className="w-full md:w-auto flex place-content-center my-8">
-                <Logo />
-              </div>
-              <ul
-                className={`w-4/5 mx-auto flex justify-between md:justify-start md:gap-x-8 mb-8 md:m-0 px-12 py-2 font-serif text-sm`}
-              >
-                <NavItem>
-                  <PrismicLink href="/" className='relative before:content-[""] before:block before:w-full before:h-[1px] before:bg-black before:absolute before:-bottom-1 before:left-0 before:origin-top-left before:scale-0 md:hover:before:scale-100 before:duration-[400ms]'>
-                    <PrismicText field={nav.data.homepageLabel} />
-                  </PrismicLink>
-                </NavItem>
-                {nav.data.links.map(item => {
-                  return (
-                  <NavItem key={asText(item.label)}>
-                    <PrismicLink field={item.link} className='relative before:content-[""] before:block before:w-full before:h-[1px] before:bg-black before:absolute before:-bottom-1 before:left-0 before:origin-top-left before:scale-0 md:hover:before:scale-100 before:duration-[400ms]'>
-                      <PrismicText field={item.label} />
-                    </PrismicLink>
-                  </NavItem>
-                )})}
-              </ul>
-            </div>
-            <div className="w-full flex place-content-center md:place-content-end font-serif text-xs">anice lab all rights reserved.</div>
-          </div>
-        </footer>
-      )}
-    </>
+          <ul
+            className={`w-4/5 mx-auto flex justify-between md:justify-start md:gap-x-8 mb-8 md:m-0 px-12 py-2 font-serif text-sm`}
+          >
+            <NavItem>
+              <PrismicLink href="/" className='relative before:content-[""] before:block before:w-full before:h-[1px] before:bg-black before:absolute before:-bottom-1 before:left-0 before:origin-top-left before:scale-0 md:hover:before:scale-100 before:duration-[400ms]'>
+                <PrismicText field={nav.data.homepageLabel} />
+              </PrismicLink>
+            </NavItem>
+            {nav.data.links.map(item => {
+              return (
+              <NavItem key={asText(item.label)}>
+                <PrismicLink field={item.link} className='relative before:content-[""] before:block before:w-full before:h-[1px] before:bg-black before:absolute before:-bottom-1 before:left-0 before:origin-top-left before:scale-0 md:hover:before:scale-100 before:duration-[400ms]'>
+                  <PrismicText field={item.label} />
+                </PrismicLink>
+              </NavItem>
+            )})}
+          </ul>
+        </div>
+        <div className="w-full flex place-content-center md:place-content-end font-serif text-xs">anice lab all rights reserved.</div>
+      </div>
+    </footer>
   )
 }
