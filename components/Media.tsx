739f0587bb9c5780961403b9d97674a2f88cbe7a
Kazuho Ishida <k10700110@gmail.com>
2023-01-27 02:21:15 +0900

    :bug: use default img tag for loading gif

--- components/Media.tsx
+++ components/Media.tsx
@@ -1,27 +1,20 @@
-import Image, { type ImageProps } from 'next/image'
-import type { FilledLinkToMediaField } from '@prismicio/types'
+import Image, { type ImageProps } from "next/image"
+import type { FilledLinkToMediaField } from "@prismicio/types"
 
 type Props = Partial<ImageProps> & {
   field: FilledLinkToMediaField
   className?: string
 }
 
-export const Media = ({
-  field,
-  className='w-full h-[50vh]',
-}: Props ) =>
-{
-  if( field.link_type !== 'Media' ) {
+export const Media = ({ field, className = "w-full h-[50vh]" }: Props) => {
+  if (field.link_type !== "Media") {
     return <></>
   }
-  if( field.kind === 'image' ) {
-    const ratio = 'auto'
+  if (field.kind === "image") {
+    const ratio = "auto"
     return (
-      <div
-        className={`relative aspect-[var(--img-ratio)] ${className}`}
-        style={{'--img-ratio': `${ratio}`}}
-      >
-        <Image src={field.url} alt={field.name} fill className='object-cover' />
+      <div className={`relative aspect-[var(--img-ratio)] ${className}`} style={{ "--img-ratio": `${ratio}` }}>
+        {field.url.endsWith(".gif") ? <img src={field.url} alt={field.name} className="object-cover" /> : <Image src={field.url} alt={field.name} fill className="object-cover" />}
       </div>
     )
   } else {
